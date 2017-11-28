ba5a8f9cdb3d5e02002b3c0655e81e8ecc9010d2
Alexandre Arribert <arribeal@iut1i-100-a12.local>
2017-11-28 11:42:24 +0100

    v0.8
    Gestion de l’état du GPS

--- app/src/main/java/com/example/arribeal/animap/MenuPrincipal.java
+++ app/src/main/java/com/example/arribeal/animap/MenuPrincipal.java
@@ -1,21 +1,43 @@
 package com.example.arribeal.animap;
 
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MenuPrincipal extends AppCompatActivity {
     private Button boutonContinent;
+    private Button boutonGeolocalisation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_principal);
+
+        final LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+
+        boutonGeolocalisation = (Button)findViewById(R.id.buttonAutourDeMoi);
+        boutonGeolocalisation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+                    Toast.makeText(getApplicationContext(), "Le GPS est actif", Toast.LENGTH_SHORT).show();
+                }else{
+                    showGPSDisabledAlertToUser();
+                }
+
+            }
+        });
 
         boutonContinent = (Button)findViewById(R.id.buttonContinent);
         boutonContinent.setOnClickListener(new View.OnClickListener() {
@@ -32,4 +54,28 @@ public class MenuPrincipal extends AppCompatActivity {
     {
 
     }
+
+    private void showGPSDisabledAlertToUser(){
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setMessage("La géolocalisation est désactivée. Voulez-vous allez dans les paramètres du GPS pour l'activer ?")
+                .setTitle("Géolocalisation")
+                .setCancelable(false)
+                .setPositiveButton("Accéder au GPS",
+                        new DialogInterface.OnClickListener(){
+                            public void onClick(DialogInterface dialog, int id){
+                                Intent callGPSSettingIntent = new Intent(
+                                        android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                                startActivity(callGPSSettingIntent);
+                            }
+                        });
+        alertDialogBuilder.setNegativeButton("Annuler",
+                new DialogInterface.OnClickListener(){
+                    public void onClick(DialogInterface dialog, int id){
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = alertDialogBuilder.create();
+        alert.show();
+    }
 }
+
