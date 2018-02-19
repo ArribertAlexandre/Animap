2b7fabd19e73a6f781b1e0ee7c8651b183e20545
Alexandre ARRIBERT <alexandrearribert@MacBook-Pro-de-Alexandre.local>
2018-02-19 18:15:29 +0100

    v0.9
    Ajout de la barre de chargement sur l'écran d'accueil

--- app/src/main/java/com/example/arribeal/animap/SplashScreenEarth.java
+++ app/src/main/java/com/example/arribeal/animap/SplashScreenEarth.java
@@ -5,21 +5,32 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class SplashScreenEarth extends AppCompatActivity {
+
+    int progress = 0;
+    ProgressBar simpleProgressBar;
+    private TextView pourcent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         requestWindowFeature(Window.FEATURE_NO_TITLE);getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen_earth);
+        simpleProgressBar = (ProgressBar) findViewById(R.id.progressBarDown);
+
+        setProgressValue(progress);
+
 
         Thread myThread = new Thread()
         {
             @Override
             public void run() {
                 try {
-                    sleep(5000);
+                    sleep(5300);
                     Intent intent = new Intent(getApplicationContext(), SplashScreenText.class);
                     startActivity(intent);
                     finish();
@@ -35,5 +46,24 @@ public class SplashScreenEarth extends AppCompatActivity {
     public void onBackPressed()
     {
 
+    }
+
+    private void setProgressValue(final int progress) {
+
+        // set the progress
+        simpleProgressBar.setProgress(progress);
+        // thread is used to change the progress value
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                setProgressValue(progress + 2);
+            }
+        });
+        thread.start();
     }
 }
